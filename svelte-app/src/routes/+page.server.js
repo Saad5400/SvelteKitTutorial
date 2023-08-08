9179078cb7b44e5d9148d6a75295b2bb75c70b17
Saad5400 <sdbtwa@gmail.com>
2023-08-08 23:26:35 +0300

    removed home layout

--- svelte-app/src/routes/+page.server.js
+++ svelte-app/src/routes/+page.server.js
@@ -1,5 +1,5 @@
-import { redirect } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
 
-export function load() {
-	throw redirect(308, '/home');
-}
+// export function load() {
+// 	throw redirect(308, '/home');
+// }
